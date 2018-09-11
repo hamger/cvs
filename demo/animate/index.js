322f57ab5051866065f0fdab07e6eaeb6a2dd42a
shanjia <shanjia@maihaoche.com>
2018-09-11 16:46:43 +0800

    add animate

--- demo/animate/index.js
+++ demo/animate/index.js
@@ -2,50 +2,52 @@ import { Canvas, Circle } from '@'
 let canvas = new Canvas({
   container: document.getElementById('container')
 })
-const dotSpeed = 1
-const dotCount = 80
+const dotCount = 3
 let r = 10
 
 let dots = []
 for (var i = 0; i < dotCount; i++) {
   r = r + 0.5
-  var x = Math.random() * (canvas.width - 2 * r) + r
-  var y = Math.random() * (canvas.height - 2 * r) + r
-  // (Math.random() * 2 - 1) 用来表示随机的运动方向
-  var xa = (Math.random() * 2 - 1) * dotSpeed
-  var ya = (Math.random() * 2 - 1) * dotSpeed
-  dots.push({
+  var x = Math.random() * canvas.width
+  var y = Math.random() * canvas.height
+  let dot = new Circle({
+    angle: 0,
     zIndex: Math.random() * dotCount,
-    r: r,
-    x: x,
-    y: y,
-    xa: xa,
-    ya: ya,
-    color: `rgba(${Math.random() * 255}, ${Math.random() *
-      255}, ${Math.random() * 255}, ${Math.random()})`
+    r,
+    x,
+    y,
+    fill: 'red'
   })
+  dots.push(dot)
 }
 
 function move () {
   canvas.remove()
-  dots.forEach(dot => {
-    // 粒子位移
-    dot.x += dot.xa
-    dot.y += dot.ya
-
-    let r = dot.r
-    // 遇到边界将加速度反向
-    dot.xa *= dot.x > canvas.width - r || dot.x < r ? -1 : 1
-    dot.ya *= dot.y > canvas.height - r || dot.y < r ? -1 : 1
-
-    canvas.add(
-      new Circle({
-        zIndex: dot.zIndex,
-        r: r,
-        x: dot.x,
-        y: dot.y,
-        fill: dot.color
+  dots.forEach((dot, i) => {
+    if (i === 0) {
+      dot.circling({
+        vpx: canvas.width / 2,
+        vpy: canvas.height / 2,
+        r: 200,
+        speed: 0.01
       })
+    } else if (i === 1) {
+      dot.elliptic({
+        vpx: canvas.width / 2,
+        vpy: canvas.height / 2,
+        radiusX: 400,
+        radiusY: 200,
+        speed: 0.03
+      })
+    } else {
+      dot.line({
+        endX: canvas.width / 2,
+        endY: canvas.height / 2,
+        time: 1000
+      })
+    }
+    canvas.add(
+      dot
     )
   })
   canvas.draw()
