cf504e8b0745688cffe43c27186f39624cbc192e
hamger <2313303800@qq.com>
2018-11-15 18:23:38 +0800

    opti

---
+++ docs/.vuepress/components/c-tool.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <Container>
+          <div id="text-container" style="width:100%;height:600px"></div>
+      </Container>
+    </div>
+</template>
+<script>
+import { Cvs, Circle } from '../../../src/'
+import Container from '../Container'
+export default {
+  components: {
+    Container
+  },
+  data() {
+    return {}
+  },
+  mounted() {
+    let cvs = new Cvs({
+      container: document.getElementById('text-container')
+    })
+    cvs.add(
+      new Circle({
+        r: 50,
+        x: 100,
+        y: 60
+      })
+    )
+    cvs.draw()
+  }
+}
+</script>
